dd111b53c856d40e6418f564949c0aaa8acafc27
Michael Armstrong <michael@m80arm.co.uk>
2020-06-27 18:22:08 +0100

    Started adding VMC Content

--- main.tf
+++ main.tf
@@ -97,3 +97,8 @@ resource "aws_route" "vcp1_london_IGW_route" {
 /*================
 Create VMC SDDC in London
 =================*/
+
+provider "vmc" {
+  refresh_token = var.vmc_api_token
+  org_id = var.vmc_org_id
+}
