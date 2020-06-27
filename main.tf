9d39b25166689b17d165ad9aa0bee8988f7d17b3
Michael Armstrong <michael@m80arm.co.uk>
2020-06-27 18:01:53 +0100

    Removed Frankfurt and updated variables

---
+++ main.tf
@@ -0,0 +1,99 @@
+/*================
+AWS Providers and Regions
+=================*/
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  alias = "london"
+  region     = var.region["london"]
+}
+
+/*================
+Create MA-VPC-01 in London
+=================*/
+resource "aws_vpc" "vpc1_london" {
+  provider             = aws.london
+  cidr_block           = var.my_london_subnets["vpc1_london"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "MA-VPC-01"
+  }
+}
+
+resource "aws_subnet" "vpc1_london_subnet1" {
+  provider                = aws.london
+  vpc_id                  = aws_vpc.vpc1_london.id
+  cidr_block              = var.my_london_subnets["vpc1_london_subnet1"]
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MA-VPC-01-SN-01"
+  }
+}
+
+resource "aws_route_table_association" "vpc1_london_subnet1_routetable" {
+  provider       = aws.london
+  subnet_id      = aws_subnet.vpc1_london_subnet1.id
+  route_table_id = aws_vpc.vpc1_london.default_route_table_id
+}
+
+resource "aws_subnet" "vpc1_london_subnet2" {
+  provider                = aws.london
+  vpc_id                  = aws_vpc.vpc1_london.id
+  cidr_block              = var.my_london_subnets["vpc1_london_subnet2"]
+  availability_zone       = "eu-west-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MA-VPC-01-SN-02"
+  }
+}
+
+resource "aws_route_table_association" "vpc1_london_subnet2_routetable" {
+  provider       = aws.london
+  subnet_id      = aws_subnet.vpc1_london_subnet2.id
+  route_table_id = aws_vpc.vpc1_london.default_route_table_id
+}
+
+resource "aws_subnet" "vpc1_london_subnet3" {
+  provider                = aws.london
+  vpc_id                  = aws_vpc.vpc1_london.id
+  cidr_block              = var.my_london_subnets["vpc1_london_subnet3"]
+  availability_zone       = "eu-west-2c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MA-VPC-01-SN-03"
+  }
+}
+
+resource "aws_route_table_association" "vpc1_london_subnet3_routetable" {
+  provider       = aws.london
+  subnet_id      = aws_subnet.vpc1_london_subnet3.id
+  route_table_id = aws_vpc.vpc1_london.default_route_table_id
+}
+
+resource "aws_default_route_table" "vpc1_london_routetable" {
+  provider               = aws.london
+  default_route_table_id = aws_vpc.vpc1_london.default_route_table_id
+  tags = {
+    Name = "MA-VPC-01-DefaultRouteTable"
+  }
+}
+resource "aws_internet_gateway" "vpc1_london_IGW" {
+  provider = aws.london
+  vpc_id   = aws_vpc.vpc1_london.id
+  tags = {
+    Name = "MA-VPC-01-InternetGateway"
+  }
+}
+
+resource "aws_route" "vcp1_london_IGW_route" {
+  provider               = aws.london
+  route_table_id         = aws_vpc.vpc1_london.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc1_london_IGW.id
+}
+/*================
+Create VMC SDDC in London
+=================*/
---
+++ variables.tf
@@ -0,0 +1,39 @@
+/*================
+AWS Credentials, region and Key-pair in that region
+=================*/
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+}
+
+variable "region" {
+  default = {
+    london    = "eu-west-2"
+    frankfurt = "eu-central-1"
+  }
+}
+variable "aws_key_pairs" {
+  default = {
+    london    = "MA-London-KeyPair"
+    Frankfurt = "MA-Frankfurt-KeyPair"
+  }
+
+}
+variable "my_london_subnets" {
+  default = {
+    vpc1_london         = "10.101.0.0/16"
+    vpc1_london_subnet1 = "10.101.1.0/24"
+    vpc1_london_subnet2 = "10.101.2.0/24"
+    vpc1_london_subnet3 = "10.101.3.0/24"
+  }
+}
+variable "my_frankfurt_subnets" {
+  default = {
+    vpc1_frankfurt         = "10.101.0.0/16"
+    vpc1_frankfurt_subnet1 = "10.101.1.0/24"
+    vpc1_frankfurt_subnet2 = "10.101.2.0/24"
+    vpc1_frankfurt_subnet3 = "10.101.3.0/24"
+  }
+}
+  
