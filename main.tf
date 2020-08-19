c6ea708b2ca6248d306de33e196f71a620d73302
Michael Armstrong <michael@m80arm.co.uk>
2020-08-19 17:57:39 +0100

    removed spaces

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   alias = "london"
-  region     = var.region["london"]
+  region = var.region["london"]
 }
 
 /*================
